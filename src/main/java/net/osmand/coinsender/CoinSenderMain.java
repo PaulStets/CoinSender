08d8e429dd2856385dd89ff9e61695fb5c0fdbbe
PaulStets <stetsenko.pavlo@gmail.com>
2017-06-10 18:55:12 +0300

    Added --prepare and --send args.

--- src/main/java/net/osmand/coinsender/CoinSenderMain.java
+++ src/main/java/net/osmand/coinsender/CoinSenderMain.java
@@ -4,8 +4,10 @@ import com.google.gson.Gson;
 import com.google.gson.internal.LinkedTreeMap;
 import com.google.gson.stream.JsonReader;
 import net.osmand.coinsender.APIException;
+import net.osmand.coinsender.wallet.PaymentResponse;
 import net.osmand.coinsender.wallet.Wallet;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -20,17 +22,26 @@ public class CoinSenderMain {
 
     private static String guid;
     private static String pass;
+    private static String directory;
 
     private static Map<String, Object> recipients;
 
     public static void main(String args[]) throws IOException {
+
+        if (args.length <= 0) {
+            System.out.println("Usage: --prepare | --send");
+            return;
+        }
+
 
         System.out.print("Enter your wallet ID: ");
         Scanner in = new Scanner(System.in);
         guid = in.nextLine();
         System.out.print("Enter your password: ");
         pass = in.nextLine();
-        in.close();
+        System.out.print("Enter full path to JSON file: ");
+        directory = in.nextLine();
+
 
         Wallet myWallet = new Wallet(SERVICE_URL, null, guid, pass);
         recipients = new HashMap<>();
@@ -42,11 +53,19 @@ public class CoinSenderMain {
         }
 
         Gson gson = new Gson();
-        JsonReader reader = new JsonReader(new FileReader(System.getProperty("user.home")
-                + "/" + "report-2017-05.json"));
+        File file = new File(directory);
+        if (!file.exists()) {
+            while (!file.exists()) {
+                System.out.print("You have entered incorrect file path. Please try again: ");
+                directory = in.nextLine();
+                file = new File(directory);
+            }
+        }
+        JsonReader reader = new JsonReader(new FileReader(directory));
         recipients.putAll((Map) gson.fromJson(reader, Map.class));
         List<LinkedTreeMap> paymentsList = (ArrayList) recipients.get("payments");
         Map<String, Long> payments = new HashMap<>();
+        long allMoney = 0;
         for (LinkedTreeMap map : paymentsList) {
             Double sum = (Double) map.get("btc");
             Double satoshi = sum / 0.00000001;
@@ -54,16 +73,54 @@ public class CoinSenderMain {
             if (payments.containsKey(map.get("btcaddress"))) {
                 inSatoshi += payments.get(map.get("btcaddress"));
             }
+            allMoney += inSatoshi;
             payments.put((String) map.get("btcaddress"), inSatoshi);
         }
         List<Map> splitPayment = splitResults(payments);
-        for (Map<String, Long> part : splitPayment) {
-            try {
-                System.out.println(myWallet.sendMany(part, null, null, "OSM Live payment"));
-            } catch (APIException e) {
-                e.printStackTrace();
+        if (args.length > 0) {
+            if (args[0].equals("--prepare")) {
+                System.out.println("Number of chunks: " + splitPayment.size());
+                System.out.println("Total sum in BTC: " + (Double) (allMoney * 0.00000001));
             }
+            else if (args[0].equals("--send")) {
+                boolean done = false;
+                while (!done) {
+                    System.out.print("Enter the number of chunk you want to pay for ('-1' to exit): ");
+                    int chunk = in.nextInt();
+                    if (chunk == -1) {
+                        break;
+                    }
+                    while (chunk < 0 || chunk >= splitPayment.size()) {
+                        System.out.print("Please enter a number between 0 and " + splitPayment.size() + ": ");
+                        chunk = in.nextInt();
+                    }
+                    Map<String, Long> currentPayment = splitPayment.get(chunk);
+                    System.out.println("All payments: ");
+                    Long total = 0l;
+                    for (String key : currentPayment.keySet()) {
+                        total += currentPayment.get(key);
+                        System.out.println("Address: " + key + ", BTC: " + (currentPayment.get(key) * 0.00000001));
+                    }
+                    System.out.print("Are you sure yo want to pay " + (Double) (total * 0.00000001) + " BTC? [y/n]: ");
+                    String answer = in.nextLine();
+                    if (!answer.toLowerCase().equals("y")) {
+                        break;
+                    }
+                    System.out.println("Paying for chunk " + chunk + "...");
+                    PaymentResponse paymentResponse = null;
+                    try {
+                        paymentResponse = myWallet.sendMany(currentPayment, null, null, "OSM Live payment");
+                    } catch (APIException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(paymentResponse.getMessage());
+                    System.out.println("Transaction Successful!");
+
+                }
+            }
+
         }
+
 
     }
 
