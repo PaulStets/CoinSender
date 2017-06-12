7c95cd88d448f819e23288e7f36f9a7479fa80ba
PaulStets <stetsenko.pavlo@gmail.com>
2017-06-12 16:37:29 +0300

    Transactions are saved in the same order as in json

--- src/main/java/net/osmand/coinsender/CoinSenderMain.java
+++ src/main/java/net/osmand/coinsender/CoinSenderMain.java
@@ -24,6 +24,8 @@ public class CoinSenderMain {
     private static String pass;
     private static String directory;
 
+    private static final String FROM_ADDRESS = "1GRgEnKujorJJ9VBa76g8cp3sfoWtQqSs4";
+
     private static Map<String, Object> recipients;
 
     public static void main(String args[]) throws IOException {
@@ -44,7 +46,7 @@ public class CoinSenderMain {
 
 
         Wallet myWallet = new Wallet(SERVICE_URL, null, guid, pass);
-        recipients = new HashMap<>();
+        recipients = new LinkedHashMap<>();
 
         try {
             System.out.println("Balance: " + String.valueOf(myWallet.getBalance()));
@@ -64,23 +66,25 @@ public class CoinSenderMain {
         JsonReader reader = new JsonReader(new FileReader(directory));
         recipients.putAll((Map) gson.fromJson(reader, Map.class));
         List<LinkedTreeMap> paymentsList = (ArrayList) recipients.get("payments");
-        Map<String, Long> payments = new HashMap<>();
+        Map<String, Long> payments = new LinkedHashMap<>();
         long allMoney = 0;
         for (LinkedTreeMap map : paymentsList) {
             Double sum = (Double) map.get("btc");
             Double satoshi = sum / 0.00000001;
             Long inSatoshi = convertToSatoshi(satoshi);
+            allMoney += inSatoshi;
             if (payments.containsKey(map.get("btcaddress"))) {
                 inSatoshi += payments.get(map.get("btcaddress"));
             }
-            allMoney += inSatoshi;
+
             payments.put((String) map.get("btcaddress"), inSatoshi);
         }
         List<Map> splitPayment = splitResults(payments);
         if (args.length > 0) {
             if (args[0].equals("--prepare")) {
+                String totalSum = String.format("%.12f", (allMoney * 0.00000001));
                 System.out.println("Number of chunks: " + splitPayment.size());
-                System.out.println("Total sum in BTC: " + (Double) (allMoney * 0.00000001));
+                System.out.println("Total sum in BTC: " + totalSum);
             }
             else if (args[0].equals("--send")) {
                 boolean done = false;
@@ -93,19 +97,20 @@ public class CoinSenderMain {
                         break;
                     }
                     while (chunk < 0 || chunk >= splitPayment.size()) {
-                        System.out.print("Please enter a number between 0 and " + splitPayment.size() + ": ");
+                        System.out.print("Please enter a number between 0 and " + (splitPayment.size() - 1) + ": ");
                         chunk = in.nextInt();
                     }
-                    paidChunks.add(chunk);
                     Map<String, Long> currentPayment = splitPayment.get(chunk);
                     System.out.println("All payments: ");
                     Long total = 0l;
                     for (String key : currentPayment.keySet()) {
                         total += currentPayment.get(key);
-                        System.out.println("Address: " + key + ", BTC: " + (currentPayment.get(key) * 0.00000001));
+                        String currentPaymentString = String.format("%.12f", currentPayment.get(key) * 0.00000001);
+                        System.out.println("Address: " + key + ", BTC: " + currentPaymentString);
                     }
                     Scanner scanner = new Scanner(System.in);
-                    System.out.print("Are you sure you want to pay " + (total * 0.00000001) + " BTC? [y/n]: ");
+                    String totalString = String.format("%.12f", (total * 0.00000001));
+                    System.out.print("Are you sure you want to pay " + totalString + " BTC? [y/n]: ");
                     String answer = scanner.nextLine();
 
                     if (!answer.toLowerCase().equals("y")) {
@@ -114,11 +119,12 @@ public class CoinSenderMain {
                     System.out.println("Paying for chunk " + chunk + "...");
                     PaymentResponse paymentResponse = null;
                     try {
-                        paymentResponse = myWallet.sendMany(currentPayment, null, null, "OSM Live payment");
+                        paymentResponse = myWallet.sendMany(currentPayment, FROM_ADDRESS, null, null);
                     } catch (APIException e) {
                         e.printStackTrace();
                     }
                     if (paymentResponse != null) {
+                        paidChunks.add(chunk);
                         System.out.println(paymentResponse.getMessage());
                         System.out.println("Transaction Successful!");
                         System.out.println("You have paid for these chunks: " + paidChunks.toString());
@@ -141,13 +147,13 @@ public class CoinSenderMain {
 
     private static List splitResults(Map<String, Long> map) {
         List<Map<String, Long>> res = new ArrayList<>();
-        Map<String, Long> part = new HashMap<>();
+        Map<String, Long> part = new LinkedHashMap<>();
         int count = 1;
         for (String key : map.keySet()) {
             part.put(key, map.get(key));
             if (part.size() == 50 || count == map.keySet().size()) {
                 res.add(part);
-                part = new HashMap<>();
+                part = new LinkedHashMap<>();
             }
             count++;
         }
